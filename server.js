c08ea1ce35e0c47a60facb99ef686c9fd292f44a
satya7289 <satyaprakashaman60@gmail.com>
2020-08-07 20:59:25 +0530

    add url

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express()
 
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
+app.get('/webshot',(req, res, next)=>{
+    res.send("hiiii");
+});
+
+
 app.listen(process.env.PORT || 8080, ()=>console.log('Server is running...'));
+
+module.exports = app;
