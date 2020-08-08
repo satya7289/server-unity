f09fc12bf998e55301b09920344a100014f0ccf9
satya7289 <satyaprakashaman60@gmail.com>
2020-08-08 13:51:46 +0530

    update for puppeteer

--- server.js
+++ server.js
@@ -1,4 +1,56 @@
 const express = require('express');
+const app = express();
+const puppeteer = require('puppeteer');
+const port = process.env.PORT || 8080;
+const validUrl = require('valid-url');
+
+var parseUrl = function(url) {
+    url = decodeURIComponent(url)
+    if (!/^(?:f|ht)tps?\:\/\//.test(url)) {
+        url = 'http://' + url;
+    }
+
+    return url;
+};
+
+app.get('/', function(req,res){
+    res.send("Hello Mr. Satya Prakash Sharma.");
+})
+
+app.get('/webshot', function(req, res) {
+    var urlToScreenshot = req.query.url;
+    if (urlToScreenshot == undefined) urlToScreenshot = 'https://google.com';
+    urlToScreenshot = parseUrl(urlToScreenshot);
+    if (validUrl.isWebUri(urlToScreenshot)) {
+        console.log('Screenshotting: ' + urlToScreenshot);
+        (async() => {
+            const browser = await puppeteer.launch({
+                args: ['--no-sandbox', '--disable-setuid-sandbox']
+            });
+
+            const page = await browser.newPage();
+            await page.goto(urlToScreenshot);
+            await page.screenshot().then(function(buffer) {
+                res.setHeader('Content-Disposition', 'attachment;filename="' + urlToScreenshot + '.png"');
+                res.setHeader('Content-Type', 'image/png');
+                res.send(buffer)
+            });
+
+            await browser.close();
+        })();
+    } else {
+        res.send('Invalid url: ' + urlToScreenshot);
+    }
+
+});
+
+app.listen(port, function() {
+    console.log('App listening on port ' + port)
+})
+
+
+/*
+const express = require('express');
 // var webshot = require('webshot');
 const captureWebsite = require('capture-website');
 const app = express()
@@ -25,3 +77,4 @@ app.get('/webshot',(req, res, next)=>{
 app.listen(process.env.PORT || 8080, ()=>console.log('Server is running...'));
 
 module.exports = app;
+*/
